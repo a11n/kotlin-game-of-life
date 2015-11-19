709ef77c4e382360e59ce1129455f9aa9cc786e8
a11n <andre.diermann@gmail.com>
2015-11-19 19:52:01 +0100

    Added test for second and third rule (ok and overpopulation).

--- src/Cell.kt
+++ src/Cell.kt
@@ -1,5 +1,13 @@
 class Cell(var isAlive: Boolean) {
+    val neighbors: MutableList<Cell> = arrayListOf()
+
     fun evolve() {
-        isAlive = false
+        val livingNeighborsCount = neighbors.filter { it.isAlive }.count()
+        isAlive = when(livingNeighborsCount) { 
+            0, 1 -> false
+            2, 3 -> true
+            else -> false
+        }
+        
     }
 }
--- test/CellTest.kt
+++ test/CellTest.kt
@@ -1,9 +1,32 @@
 import org.junit.Test
 import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class CellTest {
     @Test fun cellShouldDieWhenUnderpopulation() {
         val cell = Cell(true)
+
+        cell.evolve()
+
+        assertFalse(cell.isAlive)
+    }
+
+    @Test fun cellShouldLiveWhenTwoOrThreeLivingNeighbors() {
+        val cell = Cell(true)
+        cell.neighbors.add(Cell(true))
+        cell.neighbors.add(Cell(true))
+        
+        cell.evolve()
+        
+        assertTrue(cell.isAlive)
+    }
+
+    @Test fun cellShouldDieWhenOverpopulation() {
+        val cell = Cell(true)
+        cell.neighbors.add(Cell(true))
+        cell.neighbors.add(Cell(true))
+        cell.neighbors.add(Cell(true))
+        cell.neighbors.add(Cell(true))
 
         cell.evolve()
 
