878c9c1e0fa38ac90e68fb1e27d7ee6568e46547
a11n <andre.diermann@gmail.com>
2015-11-22 10:26:00 +0100

    Enhanced evolve() signature to represent the "natural" representation of Cell evolution (the prior implementation was stronger related to evolution being triggered from Universe)

--- src/Cell.kt
+++ src/Cell.kt
@@ -1,7 +1,7 @@
 class Cell(var isAlive : Boolean = false) {
     val neighbors: MutableList<Cell> = arrayListOf()
 
-    fun evolve(livingNeighborsCount: Int) {
+    fun evolve(livingNeighborsCount: Int = neighbors.count { it.isAlive }) {
         isAlive = when(livingNeighborsCount) { 
             0, 1 -> false
             2 -> isAlive
--- test/CellTest.kt
+++ test/CellTest.kt
@@ -6,31 +6,40 @@ class CellTest {
     @Test fun cellShouldDieWhenUnderpopulation() {
         val cell = Cell(true)
 
-        cell.evolve(livingNeighborsCount = 0)
+        cell.evolve()
 
         assertFalse(cell.isAlive)
     }
 
     @Test fun cellShouldLiveWhenTwoOrThreeLivingNeighbors() {
         val cell = Cell(true)
+        cell.neighbors.add(Cell(true))
+        cell.neighbors.add(Cell(true))
         
-        cell.evolve(livingNeighborsCount = 2)
+        cell.evolve()
         
         assertTrue(cell.isAlive)
     }
 
     @Test fun cellShouldDieWhenOverpopulation() {
         val cell = Cell(true)
+        cell.neighbors.add(Cell(true))
+        cell.neighbors.add(Cell(true))
+        cell.neighbors.add(Cell(true))
+        cell.neighbors.add(Cell(true))
         
-        cell.evolve(livingNeighborsCount = 4)
+        cell.evolve()
 
         assertFalse(cell.isAlive)
     }
 
     @Test fun cellShouldRebornWhenThreeLivingNeighbors() {
         val cell = Cell(false)
+        cell.neighbors.add(Cell(true))
+        cell.neighbors.add(Cell(true))
+        cell.neighbors.add(Cell(true))
 
-        cell.evolve(livingNeighborsCount = 3)
+        cell.evolve()
 
         assertTrue(cell.isAlive)
     }
